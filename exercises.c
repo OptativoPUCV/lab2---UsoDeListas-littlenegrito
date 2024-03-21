67e627f80191a02afda8679b3ddc0534e7967569
Your Name <you@example.com>
2024-03-21 20:58:27 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -129,7 +129,6 @@ int parentesisBalanceados(char *cadena) {
       }else if(cadena[i] == ')'|| cadena[i] == ']'|| cadena[i] == '}'){
         if(stack == NULL) return 1;
         void* cierre = top(stack);
-        cierre = (char*) cierre;
 
         if((cadena[i] == ')' && cierre == '(' ) || (cadena[i] == ']' && cierre == '[') || (cadena[i] == '}' && cierre == '{')){
           pop(stack);
